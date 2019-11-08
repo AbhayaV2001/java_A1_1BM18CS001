cc5ccfd241f47559897f475e42ca2af18f88a334
AbhayaV2001 <53890164+AbhayaV2001@users.noreply.github.com>
2019-11-08 15:05:01 +0530

    Create Student_pk.java

---
+++ Student_pk.java
@@ -0,0 +1,32 @@
+
+package CIE;
+
+
+
+import java.util.*;
+
+
+
+public class Student_pk {
+
+    String name, usn;
+
+    int sem;
+
+    
+
+    Student() {
+
+        System.out.println("Enter Name, USN, Sem");
+
+        Scanner sc = new Scanner(System.in);
+
+        name = sc.next();
+
+        usn = sc.next();
+
+        sem = sc.nextInt();
+
+    }
+
+}
